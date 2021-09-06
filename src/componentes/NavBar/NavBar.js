c37c48eb7c3b7ae85653a7216aa0f0c77ac53fcb
Aldo Baldi <aldobaldi15@gmail.com>
2021-09-06 20:19:03 -0300

    Cambio lugar carpeta

--- src/componentes/NavBar/NavBar.js
+++ src/componentes/NavBar/NavBar.js
@@ -5,13 +5,17 @@ import './NavBar.css';
 export const NavBar = () => {
     return (
         <div className="navbar">
-            <a><img src="multimedia/" alt="Logo"></img></a>
+            <a><img src="./public/multimedia/svg_icons/logosvg" alt="Logo"></img></a>
             <ul>
                 <li>Servicios</li>
                 <li>Tienda</li>
                 <li>Nosotros</li>
+                <li>Clientes</li>
             </ul>
+            <span>Login</span>
+            
         </div>
     )
 }
+
 export default NavBar
