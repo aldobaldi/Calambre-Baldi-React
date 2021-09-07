f81a538969e3bf4191acc6e6fc1a37fb1d317d1f
Aldo Baldi <aldobaldi15@gmail.com>
2021-09-07 10:53:42 -0300

    agrego logo y icono de carrito

--- src/componentes/NavBar/NavBar.js
+++ src/componentes/NavBar/NavBar.js
@@ -1,21 +1,27 @@
 import React from 'react'
-import { isElement } from 'react-dom/test-utils';
 import './NavBar.css';
+import logo from '../../multimedia/svg_iconos/logosvg.svg'
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
 export const NavBar = () => {
     return (
         <div className="navbar">
-            <a><img src="./public/multimedia/svg_icons/logosvg" alt="Logo"></img></a>
+            <img 
+                src= {logo} alt="Logo Empresa" height="80" width="150">
+            </img>
             <ul>
                 <li>Servicios</li>
                 <li>Tienda</li>
                 <li>Nosotros</li>
                 <li>Clientes</li>
-            </ul>
-            <span>Login</span>
-            
+            </ul>   
+            <span>
+                <ShoppingCartIcon/>
+            </span>
         </div>
     )
 }
 
 export default NavBar
+
+
